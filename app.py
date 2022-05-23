eb666760b25de0cc83757075ad6a78fedbae0c14
LukaBatilashvili <lukabatilashvili07@gmail.com>
2022-05-23 21:22:24 +0400

    added app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_main():
+    return '<h1>Welcome ro out flask applications</h1>'
+
+@app.route('/<username>')
+def get_user(username):
+    return f'<h1>hello {username}</h1>'
+
+if __name__ == '__main__':
+    app.run()
